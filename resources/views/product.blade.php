a08c165db091088c522a192be22549f01a7613b7
Hameed Hussain <Hussain@HUSSAIN.aquarium.com>
2019-10-09 13:56:06 +0400

    Commit

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.mylayout')
 @section('middle_main')
-This is product page and test page for github
+This is product page and test page for github it is not uploaded
 <a href="http://project2.test/about">About</a>
 @endsection
