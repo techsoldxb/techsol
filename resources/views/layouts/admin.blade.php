80c1667b6173c0320849dc014cc669d331395ace
Hameed Hussain <Hussain@HUSSAIN.aquarium.com>
2019-10-14 08:34:45 +0400

    13/10/2019 Commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -80,8 +80,9 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-    <img src="dist/img/Logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
+    <img src="{{ asset('dist/img/Logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8')}}">
+
 
            
 
