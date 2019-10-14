89284111f1db72616dda8e3c94375ab56b251ff8
bhhussain <bhhussain@gmail.com>
2019-10-14 09:00:50 +0400

    Logo Image corrected

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -80,14 +80,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-    <img src="{{ asset('dist/img/Logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8')}}">
-
-
-           
-
-
-     <span class="brand-text font-weight-light">Oman</span>
+    <img src={{asset('dist/img/logo.png class=brand-image img-circle elevation-3 style=opacity: .8')}}>
+     <span class="brand-text font-weight-light">Oman Aquarium</span>
     </a>
 
     <!-- Sidebar -->
@@ -95,7 +89,8 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <img src={{asset('dist/img/user2-160x160.jpg class=img-circle elevation-2 alt=User Image')}}>
+
         </div>
         <div class="info">
         <a href="#" class="d-block">{{ Auth::user()->name }}</a>
