42a020c798b2641b91a2ad1a6866cac4226b8f15
techsoldxb <bhhussain@gmail.com>
2020-05-17 02:33:29 +0400

    Commit

--- app/Http/Controllers/Admin/AccountsController.php
+++ app/Http/Controllers/Admin/AccountsController.php
@@ -57,7 +57,7 @@ class AccountsController extends Controller
    
 
            
-    $arr['accounts'] = Account::where('th_comp_code', auth()->user()->company)->where('th_emp_id', auth()->user()->id)
+    $arr['accounts'] = Account::where('th_comp_code', auth()->user()->company)
     ->where('th_pay_status', 0)->orderBy('th_tran_no','desc')->get();
     return view('admin.accounts.index')->with($arr);    
     
