474bf62efa65aeb26d1f94e054c4f61165aed2e8
techsoldxb <bhhussain@gmail.com>
2020-07-12 18:04:39 +0400

    Commit

--- resources/views/homeicc.blade.php
+++ resources/views/homeicc.blade.php
@@ -161,97 +161,6 @@
    
 
 
-<!-- Main content -->
-<section class="content">
-      <div class="container-fluid">
-        <!-- Info boxes -->
-        <div class="row">
-          <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box">
-              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-dollar-sign"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Total Count</span>
-                <span class="info-box-number">
-
-                @if($user->user_type =='admin')         
-                
-
-                0
-
-                @else
-                0
-                @endif
-
-               
-                  
-                </span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-          <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
-              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-cog"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Cancel</span>
-                <span class="info-box-number">
-
-                  {{$booking_cancel}}
-
-                </span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-
-          <!-- fix for small devices only -->
-          <div class="clearfix hidden-md-up"></div>
-
-          <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
-              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-coins"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Accounts User</span>
-                <span class="info-box-number">0</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-          <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
-              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-users"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Users</span>
-                <span class="info-box-number">0</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-        </div>
-        <!-- /.row -->
-        </div>
-
-
-
-
-  
-
-
-<section class="content">
-
-  
   
 
 
@@ -270,7 +179,7 @@
               <div class="card-body">
                 <div class="d-flex">
                   <p class="d-flex flex-column">
-                    <span class="text-bold text-lg">{{$booking_student_count}}</span>
+                    <span class="text-bold text-lg">{{$enqkkdtotal}}</span>
                     <span>Techsol Computers - KKD</span>
                   </p>
                   <p class="ml-auto d-flex flex-column text-right">
@@ -285,7 +194,7 @@
 
                 <div class="d-flex">
                   <p class="d-flex flex-column">
-                    <span class="text-bold text-lg">{{$booking_teacher_count}}</span>
+                    <span class="text-bold text-lg">{{$enqrmdtotal}}</span>
                     <span>Bash Computers - RMD</span>
                   </p>
                   <p class="ml-auto d-flex flex-column text-right">
@@ -324,116 +233,6 @@
             </div>
             <!-- /.card -->
 
-            <div class="card">
-              <div class="card-header border-0">
-                <h3 class="card-title">Booking Summary</h3>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-sm">
-                    <i class="fas fa-download"></i>
-                  </a>
-                  <a href="#" class="btn btn-tool btn-sm">
-                    <i class="fas fa-bars"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-body p-0">
-                <table class="table table-striped table-valign-middle">
-                  <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Booking Count</th>
-                    <th>Amount</th>
-                    <th>More</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>
-                      <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                      ICC
-                    </td>
-                    <td>{{$booking_icc_count}}</td>
-                    <td>
-                      <small class="text-success mr-1">
-                        <i class="fas fa-arrow-up"></i>
-                        
-                      </small>
-                      
-                      {{ number_format($booking_icc_amount,3) }}
-                    </td>
-                    <td>
-                      <a href="#" class="text-muted">
-                        <i class="fas fa-search"></i>
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                      Manal Al Balushi
-                    </td>
-                    <td>{{$booking_manal_count}}</td>
-                    <td>
-                      <small class="text-warning mr-1">
-                        <i class="fas fa-arrow-up"></i>
-                        
-                      </small>
-                      
-                      {{ number_format($booking_manal_amount,3) }}
-                    </td>
-                    <td>
-                      <a href="#" class="text-muted">
-                        <i class="fas fa-search"></i>
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                     Gelan
-                    </td>
-                    <td>{{$booking_gelan_count}}</td>
-                    <td>
-                      <small class="text-danger mr-1">
-                        <i class="fas fa-arrow-up"></i>
-                        
-                      </small>
-                      
-                      {{ number_format($booking_gelan_amount,3) }}
-                    </td>
-                    <td>
-                      <a href="#" class="text-muted">
-                        <i class="fas fa-search"></i>
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                      FOH
-                      <span class="badge bg-danger">NEW</span>
-                    </td>
-                    <td>{{$booking_foh_count}}</td>
-                    <td>
-                      <small class="text-success mr-1">
-                        <i class="fas fa-arrow-up"></i>
-                        
-                      </small>
-                      
-
-                      {{ number_format($booking_foh_amount,3) }}
-
-                    </td>
-                    <td>
-                      <a href="#" class="text-muted">
-                        <i class="fas fa-search"></i>
-                      </a>
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
             <!-- /.card -->
           </div>
           <!-- /.col-md-6 -->
@@ -481,60 +280,6 @@
             </div>
             <!-- /.card -->
 
-            <div class="card">
-              <div class="card-header border-0">
-                <h3 class="card-title">Overview</h3>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-sm btn-tool">
-                    <i class="fas fa-download"></i>
-                  </a>
-                  <a href="#" class="btn btn-sm btn-tool">
-                    <i class="fas fa-bars"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                  <p class="text-success text-xl">
-                    <i class="fas fa-school"></i>
-                  </p>
-                  <p class="d-flex flex-column text-right">
-                    <span class="font-weight-bold">
-                      <i class="ion ion-android-arrow-up text-success"></i> 
-                      {{$booking_school_count}}
-                    </span>
-                    <span class="text-muted">School Trips</span>
-                  </p>
-                </div>
-                <!-- /.d-flex -->
-                <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                  <p class="text-warning text-xl">
-                    <i class="fas fa-building"></i>
-                  </p>
-                  <p class="d-flex flex-column text-right">
-                    <span class="font-weight-bold">
-                      <i class="ion ion-android-arrow-up text-warning"></i> 
-                      {{$booking_corporate_count}}
-                    </span>
-                    <span class="text-muted">Corporate Booking</span>
-                  </p>
-                </div>
-                <!-- /.d-flex -->
-                <div class="d-flex justify-content-between align-items-center mb-0">
-                  <p class="text-danger text-xl">
-                    <i class="fas fa-handshake"></i>
-                  </p>
-                  <p class="d-flex flex-column text-right">
-                    <span class="font-weight-bold">
-                      <i class="ion ion-android-arrow-up text-danger"></i> 
-                      {{$booking_events_count}}
-                    </span>
-                    <span class="text-muted">Events</span>
-                  </p>
-                </div>
-                <!-- /.d-flex -->
-              </div>
-            </div>
           </div>
           <!-- /.col-md-6 -->
         </div>
