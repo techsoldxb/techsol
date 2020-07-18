89ee40d49491720c7d7e97331fcf70e891bf81c6
techsoldxb <bhhussain@gmail.com>
2020-07-18 15:43:02 +0400

    Commit

--- resources/views/job/jobinvtotal/index.blade.php
+++ resources/views/job/jobinvtotal/index.blade.php
@@ -41,7 +41,7 @@
             
 
 
-            <td>{{ date('d-m-Y', strtotime($c->job_invoice_date)) }}</td>  
+            <td>{{ ($c->job_invoice_date }}</td>  
             <td>{{ number_format($c->total,3) }}</td>
             
 
