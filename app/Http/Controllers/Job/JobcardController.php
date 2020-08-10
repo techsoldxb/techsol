04e507cc441eea04ef4ae54627c9f46c5a3c35eb
techsoldxb <bhhussain@gmail.com>
2020-08-10 12:11:51 +0400

    commit

--- app/Http/Controllers/Job/JobcardController.php
+++ app/Http/Controllers/Job/JobcardController.php
@@ -183,7 +183,7 @@ class JobcardController extends Controller
         //$message = "Dear Customer: Your product $brand-$model-$type is registered for service. Your Job No is $new_id.Thank you";
         //$message = "Dear%20Customer%20Your%20product%20HP%20Pro%20Desk%20Laptop%20Job%20No%20%24jobid%20is%20repaired%20and%20ready%20to%20collect%20Thank%20you";
 
-        $message = rawurlencode('Dear Customer: Your product is registered for service. Thank you');
+        $message = rawurlencode('Dear Customer: Your product is registered for service. Please contact 9944942308 to know the status. Thank you');
 
         
         //$message = rawurlencode($message);
@@ -201,7 +201,7 @@ class JobcardController extends Controller
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       $result = curl_exec($ch); // This is the result from the API        
+        $result = curl_exec($ch); // This is the result from the API        
         curl_close($ch);
 
        // echo $result;
