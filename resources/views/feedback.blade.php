e5c89379015d2bc7296dc53d3b462ac430e8a669
techsoldxb <bhhussain@gmail.com>
2020-08-18 22:49:56 +0400

    commit

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Feedback Form HTML Template - reusable form</title>
+    <title>Techsol Feedback</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Optional theme -->
@@ -21,6 +21,10 @@
         <div class="imagebg"></div>
         <div class="row " style="margin-top: 50px">
             <div class="col-md-6 col-md-offset-3 form-container">
+                <div class="login-logo" align="center">
+                    <img src={{asset('dist/img/tclogo1.png class=img-circle elevation-2 alt=Logo')}}>
+                </div>
+
                 <h2>Feedback</h2>
                 <p> Please provide your feedback below: </p>
                 <form role="form" method="post" id="reused_form">
@@ -29,16 +33,16 @@
                             <label>How do you rate your overall experience?</label>
                             <p>
                                 <label class="radio-inline">
-                                    <input type="radio" name="experience" id="radio_experience" value="bad">
-                                    Bad
-                                </label>
-                                <label class="radio-inline">
-                                    <input type="radio" name="experience" id="radio_experience" value="average">
-                                    Average
+                                    <input type="radio" name="experience" id="radio_experience" value="excellent">
+                                    Excellent
                                 </label>
                                 <label class="radio-inline">
                                     <input type="radio" name="experience" id="radio_experience" value="good">
                                     Good
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="experience" id="radio_experience" value="poor">
+                                    Poor
                                 </label>
                             </p>
                         </div>
@@ -47,7 +51,7 @@
                         <div class="col-sm-12 form-group">
                             <label for="comments"> Comments:</label>
                             <textarea class="form-control" type="textarea" name="comments" id="comments"
-                                placeholder="Your Comments" maxlength="6000" rows="7"></textarea>
+                                placeholder="Your Comments" maxlength="6000" rows="4"></textarea>
                         </div>
                     </div>
                     <div class="row">
