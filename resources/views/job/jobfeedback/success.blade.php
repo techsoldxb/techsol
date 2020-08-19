6d4039e0d1fb32d0795b1651b1f4589cffc17983
techsoldxb <bhhussain@gmail.com>
2020-08-19 15:32:36 +0400

    Commit

--- resources/views/job/jobfeedback/success.blade.php
+++ resources/views/job/jobfeedback/success.blade.php
@@ -107,8 +107,13 @@
 
 
                 <h4 class="text-center">Feedback successfully submited</h4>
-                <h4 class="text-center">You can use the code to redeem
-                    <span class="badge badge-primary">{{$feedback->fb_coupon}}</span></h4>
+                <h4 class="text-center">You can use the below code to redeem</h4>
+                <h2 class="text-center text-primary">
+                    <mark>{{$feedback->fb_coupon}}</mark>
+
+
+                </h2>
+
 
 
 
