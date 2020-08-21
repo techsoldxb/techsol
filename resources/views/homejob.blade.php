39c2eb713b2d3b01971183ed42c5f03bb5a3d381
techsoldxb <bhhussain@gmail.com>
2020-08-21 22:01:10 +0400

    commit

--- resources/views/homejob.blade.php
+++ resources/views/homejob.blade.php
@@ -464,6 +464,20 @@
                             <tbody>
 
 
+                                @foreach ($homejob as $month => $values)
+                                <tr>
+                                    <td>{{ \Carbon\Carbon::parse($month)->format('F Y') }}</td>
+                                    @foreach ($job_comp_codes as $job_comp_code)
+                                    <td>₹{{ number_format($homejob[$month][$job_comp_code]['amount'] ?? '0') }}</td>
+
+
+
+                                    @endforeach
+                                    @foreach ($job_comp_codes as $job_comp_code)
+                                    <td>{{ $homejob[$month][$job_comp_code]['count'] ?? '0' }}</td>
+                                    @endforeach
+                                </tr>
+                                @endforeach
 
 
 
