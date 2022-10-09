de6ffb40426310bea70f92ab711c484d35ba5950
techsoldxb <bhhussain@gmail.com>
2022-10-09 19:29:28 +0400

    commit

--- resources/views/job/jobcard/show.blade.php
+++ resources/views/job/jobcard/show.blade.php
@@ -262,63 +262,6 @@
 
 
 
-                <div class="form-group">
-                    <strong>
-                        Terms & Conditions:
-                    </strong>
-                    <br>
-                    <ol>
-                        <li>The customer authorizes us to:
-                            <ol>
-                                <li type="a"> Carry out agreed work on the device ourselves</li>
-                                <li type="a"> Carry out necessary troubleshooting by using our testing devices/items in
-                                    connection with that work</li>
-
-                            </ol>
-                        </li>
-                        <li>Every effort will be taken to complete the work within the estimated time. However, we are
-                            not
-                            responsible for delays due to non-availability of parts or materials, or for any
-                            circumstances
-                            beyond our reasonable control</li>
-                        <li>Any initial estimate is based upon the visual inspection. If any additional required
-                            repair(s) are
-                            found during dismantling, a supplementary estimate will be issued for those repair(s).</li>
-                        <li>Estimates are validfor 7 days from the date of receipt. We will carry out any work only
-                            after the
-                            customer’s approval of the estimate by written or SMS to the cell phone mentioned in the job
-                            enquiry form authorization.</li>
-                        <li>Displaced/replaced parts shall be disposed of immediately unless we are instructed by the
-                            customer at the time of work authorization and must be collected along with te device. No
-                            later claim will be considered/entertained</li>
-                        <li>If we or our subcontractor inadvertently damage the device while carrying out the authorized
-                            job/agreed work, we will repair the damage in the device at our cost. We DO NOT undertake
-                            any other liability</li>
-                        <li> Once the device is available for collection, the customer will be notified by telephone,
-                            SMS or
-                            email. The customer needs tobring the original job card slip. Failing to produce the
-                            original job
-                            card slip at the time of collection will result in denial to deliver the device. If the
-                            person collecting
-                            the device in not the registered owner, he or she must provide the original job card slip or
-                            other suitable written authorization.</li>
-                        <li>Unless agreed in advance, the customer is required to fully settle the invoice amount before
-                            we return the device.</li>
-                        <li>In the event of late or non-payment of any amount owed by the customer or failure to collect
-                            the device within 15 days from the date fo job completion notification, the device will be
-                            scrapped without any further notice to the customer and any later claim for the device will
-                            not
-                            be entertained.</li>
-                        <li> On receipt of the device, the customer/person collects the device is required to inspect
-                            and
-                            notify us of any issue there itself. Should the customer take delivery of the device, it is
-                            confirmed
-                            that he/she is satisfied with the work and no subsequent claims will be
-                            entertained/accepted.</li>
-                    </ol>
-                    <p align="center">Above said terms and conditions agreed and signed.</p>
-
-                </div>
 
 
 
